966e11e61963af5358894636d20b331d8ead8fbc
JLouhela <louhela.joni@gmail.com>
2020-02-01 12:39:02 +0200

    WIP bullet paska, need to pull

--- BulletSpawner.gd
+++ BulletSpawner.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
+export var bullet_pool_size = 50
 
+onready var bullet = preload("res://Bullet.tscn")
+var bullet_pool = []
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,8 +11,21 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+    for i in range (bullet_pool_size):
+        var b = bullet.instance()
+        b.position = Vector2(-100, -100)
+        get_parent().call_deferred("add_child", b)
+        b.hide()
+        b.connect("area_entered", self, "onBulletGone" )
+        b.getnode("VisibilityNotifier2D").connect("screenexited", self, "onBulletGone" )
+        bullet_pool.append(b)
 
+func onBulletFree(bullet):
+    bullet_pool.append(bullet)
+    print("BULLET_GONE")
+    
+func _shoot():
+    var b = bullet_pool
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
