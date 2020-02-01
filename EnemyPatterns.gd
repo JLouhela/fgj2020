8b1eb2ad58a48d058afbe0bcb8e29c073a32f8cc
Pete Sevander <pete.sevander@gmail.com>
2020-02-01 13:59:04 +0200

    enemy spawner and initial pattern

---
+++ EnemyPatterns.gd
@@ -0,0 +1,19 @@
+extends Node
+
+
+var wave = {
+    "count": 5,
+    "speed": 400,
+    "shoots": false,
+    "interval": 0.5,
+    "path": load("res://enemy_paths/path1.tres")
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#    pass
