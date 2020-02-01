2dec325f79ce1f15b6e365b6f28b83984f50abaf
JLouhela <louhela.joni@gmail.com>
2020-02-01 14:11:01 +0200

    Bullet spawner

---
+++ Bullet.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+signal free_bullet
+
+export var bullet_speed = Vector2(0,-1)
+var bullet_speed_multiplier = 500
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    pass # Replace with function body.
+
+func _process(delta):
+    position += bullet_speed * bullet_speed_multiplier * delta
+    if position.y < 0:
+        emit_signal("free_bullet", self)
+
+func disable():
+    hide()
+    set_block_signals(true)
+
+func enable():
+    show()
+    set_block_signals(false)
+    
+func set_speed(s):
+    bullet_speed = s
+
+func _on_Bullet_area_entered(area):
+    emit_signal("free_bullet", self)
+
--- BulletSpawner.gd
+++ BulletSpawner.gd
@@ -1,32 +1,35 @@
 extends Node2D
 
 export var bullet_pool_size = 50
+export var fire_delay_ms = 100
 
 onready var bullet = preload("res://Bullet.tscn")
 var bullet_pool = []
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    for i in range (bullet_pool_size):
+    for _i in range (bullet_pool_size):
         var b = bullet.instance()
         b.position = Vector2(-100, -100)
-        get_parent().call_deferred("add_child", b)
-        b.hide()
-        b.connect("area_entered", self, "onBulletGone" )
-        b.getnode("VisibilityNotifier2D").connect("screenexited", self, "onBulletGone" )
+        b.set_as_toplevel(true)
+        add_child(b)
+        b.disable()
+        b.connect("free_bullet", self, "onBulletFree")
         bullet_pool.append(b)
+        $BulletTimer.wait_time = float(fire_delay_ms / 1000.0)
+        $BulletTimer.start()
 
-func onBulletFree(bullet):
-    bullet_pool.append(bullet)
-    print("BULLET_GONE")
+func onBulletFree(b):
+    bullet_pool.append(b)
     
 func _shoot():
-    var b = bullet_pool
+    var b = bullet_pool.pop_back()
+    if (!b):
+        print("BULLET POOL TOO SMALL MAYDAY")
+        return
+    b.position = global_position
+    b.enable()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
+func _on_BulletTimer_timeout():
+    _shoot()
+
